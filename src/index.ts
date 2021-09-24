bb9eb368d45faf3174211e0203f0f03651294468
sendra <polsendra@gmail.com>
2021-09-24 10:26:12 +0200

    feat: added secrets

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ import { AaveProtocolDataProviderFactory } from "./contracts/AaveProtocolDataPro
 const KEY = process.env.ALCHEMY_KEY;
 const POLYGON_ALCHEMY_KEY = process.env.POLYGON_ALCHEMY_KEY;
 const MUMBAI_ALCHEMY_KEY = process.env.MUMBAI_ALCHEMY_KEY;
-if (KEY === "" || MUMBAI_ALCHEMY_KEY === "" || POLYGON_ALCHEMY_KEY === "")
+if (
+  KEY === "" ||
+  MUMBAI_ALCHEMY_KEY === "" ||
+  POLYGON_ALCHEMY_KEY === "" ||
+  !KEY ||
+  !MUMBAI_ALCHEMY_KEY ||
+  !POLYGON_ALCHEMY_KEY
+)
   throw new Error("ENV ALCHEMY KEY not configured");
 // Get the main addreses from the DOC's https://docs.aave.com/developers/getting-started/deployed-contracts
 
